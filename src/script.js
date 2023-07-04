bd115cdc9cfb6513f2586b6182416445a2654860
JoeWebDev70 <joellemoureu@gmail.com>
2023-07-04 16:59:21 +0200

    ci: html - css - js
    conntinu on html and css for base page and on modal windows

--- src/script.js
+++ src/script.js
@@ -1,37 +1,37 @@
 import "./style.scss";
 import "./js_libs/modal_script.js";
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", function(){
-    search();
-});
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", function(){
+//     search();
+// });
 
 
-function search() {
-    let word = document.querySelector("input").value;
-    let resultat = document.querySelector("#resultat");
+// function search() {
+//     let word = document.querySelector("input").value;
+//     let resultat = document.querySelector("#resultat");
 
-    if(word.length != 0){
-        let url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
-        fetch(url).then(reponse => reponse.json().then(data =>{
-            console.log(data);
-            try{
-                // for(let words of data){
-                    // let definition = words.meanings[0].definitions[0].definition;
-                    // resultat.innerHTML = "<p class='res'> "+definitions + "</p>";
-                // }
-                for(let i = 0; i < data.length; i++){
-                    for(let j = 0; j < data[i].meanings.length; j++){
-                        console.log("data i : ",Object.values(data[i]));
-                        console.log(data[i].meanings[j]);
-                    }
+//     if(word.length != 0){
+//         let url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
+//         fetch(url).then(reponse => reponse.json().then(data =>{
+//             console.log(data);
+//             try{
+//                 // for(let words of data){
+//                     // let definition = words.meanings[0].definitions[0].definition;
+//                     // resultat.innerHTML = "<p class='res'> "+definitions + "</p>";
+//                 // }
+//                 for(let i = 0; i < data.length; i++){
+//                     for(let j = 0; j < data[i].meanings.length; j++){
+//                         console.log("data i : ",Object.values(data[i]));
+//                         console.log(data[i].meanings[j]);
+//                     }
                     
-                }
-            }catch(err){
-                resultat.innerHTML = "<p class='res'> Aucune definition trouvée ! </p>";
-            }
-        }));
-    }else{
-        resultat.innerHTML = "<p class='res'> Veuillez remplir ce champ</p>";
-    }
-}
+//                 }
+//             }catch(err){
+//                 resultat.innerHTML = "<p class='res'> Aucune definition trouvée ! </p>";
+//             }
+//         }));
+//     }else{
+//         resultat.innerHTML = "<p class='res'> Veuillez remplir ce champ</p>";
+//     }
+// }
