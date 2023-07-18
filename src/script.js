a3f76a7b3363c6d4f89aeae365494f3c90caa60c
JoeWebDev70 <joellemoureu@gmail.com>
2023-07-18 11:47:24 +0200

    ci: display error message
    javascript function for clone template and display error message if word searched is not found

--- src/script.js
+++ src/script.js
@@ -8,6 +8,9 @@ import "./js_libs/creditsFonts.js";
 const searchBar = document.querySelector("#search_bar");
 const searchZone = document.querySelector("#search_zone");
 const questions = document.querySelectorAll("dt");
+
+const resultZone = document.querySelector("#div_resultat dl");
+
 // const btn = document.querySelector("button");
 // btn.addEventListener("click", function(e){
 //     e.preventDefault();
@@ -28,43 +31,21 @@ if(searchBar != null) {
     });
 }
 
-questions.forEach(question => {
-    question.addEventListener("click", function(){
-        const elementHasActive = document.querySelector(".active");
-        // Verifier qu'on a trouver un élément et que l'élément trouvé n'est pas celui cliqué
-        if(elementHasActive && this !== elementHasActive) {
-            elementHasActive.classList.remove("active");
-        }
-        //clic à nouveau referme
-        this.classList.toggle("active");
-    });
-});
 
 
 function search(evt) {
     evt.preventDefault();
-    let word = document.querySelector("input").value;
-    let notFound = "Definition not found ! please enter an other word!"
-    // let resultat = document.querySelector("#resultat");
-    // let errorMessage = document.querySelector("input").placeholder;
-    if("content" in document.createElement("template")){
-        let temp = document.querySelector("#temp_panel");
-        const resultZone = document.querySelector("#div_resultat dl");
-        let clone = document.importNode(temp.content, true);
-        let dt = clone.querySelectorAll("dt");
-        let dl = clone.querySelectorAll("dl");
-        console.log("dt : ",dt);
-        console.log("dl : ",dl);
-    }
+    let word = searchZone.value;
+    console.log(word);
     if(word.length != 0){
         let url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
         fetch(url).then(reponse => reponse.json().then(data =>{
             console.log(data);
             try{
-                // for(let words of data){
+                for(let words of data){
                     // let definition = words.meanings[0].definitions[0].definition;
                     // resultat.innerHTML = "<p class='res'> "+definitions + "</p>";
-                // }
+                
                 for(let i = 0; i < data.length; i++){
                     for(let j = 0; j < data[i].meanings.length; j++){
                         console.log("data i : ",Object.values(data[i]));
@@ -72,10 +53,62 @@ function search(evt) {
                     }
                     
                 }
+                }
             }catch(err){
-                searchZone.placeholder.textContent = notFound;
+                console.log(err);
+                displayError();
+                
             }
         }));
     }
 }
 
+function displayError(){
+    let notFound = "Definition not found! please enter an other word!"
+    if("content" in document.createElement("template")){
+        let temp = document.querySelector("#temp_panel");
+        let clone = document.importNode(temp.content, true);
+        let dt = clone.querySelector("dt");
+        let div = clone.querySelector(".panel");
+        dt.textContent = notFound;
+        dt.classList.add("not_found");
+        div.classList.add("not_found");
+        resultZone.appendChild(clone);            
+    }
+}
+
+// function displayResult(){
+//     let notFound = "Definition not found! please enter an other word!"
+//     // let resultat = document.querySelector("#resultat");
+//     if("content" in document.createElement("template")){
+//         let temp = document.querySelector("#temp_panel");
+//         const resultZone = document.querySelector("#div_resultat dl");
+//         let clone = document.importNode(temp.content, true);
+        
+        
+//         let dts = clone.querySelectorAll("dt");
+//         // let dds = clone.querySelectorAll("dd");
+//         let divs = clone.querySelectorAll("div");
+//         // let h2 = clone.querySelector("h2");
+//         // let h3s = clone.querySelectorAll("h3");
+//         // let ps = clone.querySelectorAll("p");
+        
+//         dts[0].textContent = notFound;
+//         dts[0].classList.add("not_found");
+//         divs[0].classList.add("not_found");
+//         resultZone.appendChild(clone);            
+//     }
+// }
+
+//open or close panel
+questions.forEach(question => {
+    question.addEventListener("click", function(){
+        const elementHasActive = document.querySelector(".active");
+        // check if element is found and if the founded element isn't clicked now
+        if(elementHasActive && this !== elementHasActive) {
+            elementHasActive.classList.remove("active");
+        }
+        //click twice => close
+        this.classList.toggle("active");
+    });
+});
